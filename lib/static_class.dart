339e134eb965469eb77d9c65c55d57b9fdd633e2
Hp <humayun1278@gmail.com>
2025-03-19 16:53:04 -0400

    Ostad 3st commit Static Class

---
+++ lib/static_class.dart
@@ -0,0 +1,27 @@
+void main() {
+  var MyObj = MyClass();
+  MyObj.addThreeNumber(2,5,8);
+
+  //Static
+  MyClass.addTwoNumber(2,6);
+}
+addTwonum(var x, int y){
+  print(x+y);
+}
+addNew(var a, var b){
+  return a+b;
+}
+
+class MyClass{
+  var MyName = "Rabbil Hasan";
+  var Alphabet = ['A', 'B','C'];
+
+  //Static method
+  static addTwoNumber(int x, int y){
+    print(x+y);
+  }
+  addThreeNumber(int x, int y, int z){
+    print(x+y+z);
+  }
+
+}
