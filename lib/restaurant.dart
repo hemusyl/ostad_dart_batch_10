c0ba34ac14c313d54c8966e347f9cc28929d396a
Hp <humayun1278@gmail.com>
2025-03-24 03:56:31 +0600

    Ostad 3st commit Static Class

--- lib/restaurant.dart
+++ lib/restaurant.dart
@@ -1,8 +1,17 @@
 class Restuarant{
 
+  String name;
+  String location = 'Sylhet';
+  int id = 2023;
+
+  Restuarant(this.name);
+
   order (String item){
     print('$item Orderd')
     print('$item Served')
+    _prepareItem(item);
+    _shopping(item);
+
   }
 // private method
   _prepareItem(String item){
